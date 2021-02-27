a6f8b88010fd3e08727813f33955cbcba6fdabc2
kamrul <kamrul1157024@gmai.com>
2021-02-28 00:24:57 +0600

    Fixed Request Paremeter

--- src/main/java/com/kamrul/blog/repositories/TagRepository.java
+++ src/main/java/com/kamrul/blog/repositories/TagRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag,Long> {
