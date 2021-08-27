a4ff5a34cc72ffb360a4ffec508f470ed99a37f3
kamrul <kamrul1157024@gmail.com>
2021-08-27 10:47:12 +0600

    add getUserByJWT

--- src/main/java/com/kamrul/server/repositories/GeneralQueryRepository.java
+++ src/main/java/com/kamrul/server/repositories/GeneralQueryRepository.java
@@ -2,11 +2,17 @@ package com.kamrul.server.repositories;
 
 import com.kamrul.server.exception.ResourceNotFoundException;
 import com.kamrul.server.exception.UnauthorizedException;
+import com.kamrul.server.models.user.User;
+import com.kamrul.server.security.jwt.JWTUtil;
 import com.kamrul.server.security.models.AppUserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+import static com.kamrul.server.utils.GeneralResponseMSG.USER_NOT_FOUND_MSG;
 
 public abstract class GeneralQueryRepository {
 
@@ -14,6 +20,17 @@ public abstract class GeneralQueryRepository {
         T t=repository.findById(id)
                 .orElseThrow(()->new ResourceNotFoundException(message));
         return t;
+    }
+
+    public static User getUserByJWT(UserRepository userRepository, Optional<String> jwt) throws ResourceNotFoundException, UnauthorizedException {
+        if(jwt.isEmpty()) throw new UnauthorizedException("User not logged in or registered");
+        Long loggedInUserId= JWTUtil.getUserIdFromJwt(jwt.get());
+        User user= GeneralQueryRepository.getByID(
+                userRepository,
+                loggedInUserId,
+                USER_NOT_FOUND_MSG
+        );
+        return  user;
     }
 
     public static AppUserDetails getCurrentlyLoggedInUser() throws UnauthorizedException {
