96314f9460f47c35ee4d4e0ba6057b67b721c132
kamrul <kamrul1157024@gmail.com>
2021-08-27 22:04:45 +0600

    rename Utils to UserFixture

--- src/test/java/com/kamrul/server/MockRequest.java
+++ src/test/java/com/kamrul/server/MockRequest.java
@@ -1,6 +1,7 @@
 package com.kamrul.server;
 
 import com.github.javafaker.Faker;
+import com.kamrul.server.fixtures.UserFixture;
 import lombok.Getter;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.http.MediaType;
@@ -17,8 +18,8 @@ public class MockRequest {
     public MockRequest(MockMvc mockMvc) throws Exception {
         Faker faker = new Faker();
         this.mockMvc = mockMvc;
-        userNameAndPassword = new ImmutablePair<>(faker.name().username()+Utils.getRandomString(),Config.User.password);
-        this.jwt = Utils.loginOrRegister(mockMvc,userNameAndPassword);
+        userNameAndPassword = new ImmutablePair<>(faker.name().username()+ UserFixture.getRandomString(),Config.User.password);
+        this.jwt = UserFixture.loginOrRegister(mockMvc,userNameAndPassword);
     }
 
     public ResultActions get(String url) throws Exception {
@@ -32,7 +33,7 @@ public class MockRequest {
     public ResultActions post(String url, Object body) throws Exception {
         return this.mockMvc.perform(MockMvcRequestBuilders
                 .post(url)
-                .content(Utils.asJsonString(body))
+                .content(UserFixture.asJsonString(body))
                 .header("Authorization",String.format("Bearer %s",jwt))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
@@ -41,7 +42,7 @@ public class MockRequest {
     public ResultActions put(String url, Object body) throws Exception {
         return this.mockMvc.perform(MockMvcRequestBuilders
                 .put(url)
-                .content(Utils.asJsonString(body))
+                .content(UserFixture.asJsonString(body))
                 .header("Authorization",String.format("Bearer %s",jwt))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
@@ -50,7 +51,7 @@ public class MockRequest {
     public ResultActions delete(String url, Object body) throws Exception {
         return this.mockMvc.perform(MockMvcRequestBuilders
                 .delete(url)
-                .content(Utils.asJsonString(body))
+                .content(UserFixture.asJsonString(body))
                 .header("Authorization",String.format("Bearer %s",jwt))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
